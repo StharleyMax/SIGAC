d5866b858838a6f198daf9429d6447e7dc500d2f
Stharley Maxwell <stharleymax@gmail.com>
2022-02-11 10:04:22 -0300

    fix: implement delete column id_user migration dropFkTableClient

--- src/migrations/1644500792896-dropFkTableClient.ts
+++ src/migrations/1644500792896-dropFkTableClient.ts
@@ -1,11 +1,27 @@
-import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableForeignKey,
+  TableColumn,
+} from 'typeorm';
 
 export class dropFkTableClient1644500792896 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropForeignKey('clients', 'User');
+
+    await queryRunner.dropColumn('clients', 'id_user');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'clients',
+      new TableColumn({
+        name: 'id_user',
+        type: 'uuid',
+        isNullable: false,
+      }),
+    );
+
     await queryRunner.createForeignKey(
       'clients',
       new TableForeignKey({
