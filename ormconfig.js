cf6b386c8819845b7b4dbfcd7094f84da1f12041
Stharley Maxwell <stharleymax@gmail.com>
2022-03-07 13:40:50 -0300

    deploy heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,7 @@
+console.log('DATABASE_URL: ', process.env.DATABASE_URL);
 module.exports = {
   type: process.env.DB_TYPE,
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   entities: ['./dist/**/*.entity.js'],
   migrations: ['./dist/migrations/*.js'],
   cli: {
